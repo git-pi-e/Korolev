f433b4354d2d5660e5b756b59aff255a66637d4a
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2021-02-11 23:00:44 +0530

    Remove JQuery :)

--- public/solarsystem/scripts/planetdata.js
+++ public/solarsystem/scripts/planetdata.js
@@ -1,54 +1,51 @@
-$( window ).on( 'load', () => {
-  const solarsys = $( "#solar-system" );
-  init = () => { }
-  // Names Activity
-  $( "#data a" ).click( function ( e ) {
-    var ref = $( this ).attr( "class" );
-    solarsys.removeClass().addClass( ref );
-    $( this ).parent().find( 'a' ).removeClass( 'active' );
-    $( this ).addClass( 'active' );
-    e.preventDefault();
-  } );
-  init();
-} );
+( () => {
+  const solarsys = document.querySelector( "#solar-system" );
+  window.onload = () => {
+    document.querySelectorAll( '#data a' ).forEach( e => {
+      e.addEventListener( "click", ( ev ) => {
+        ev.preventDefault();
+        console.log( e.classList.value );
+        let ref = e.classList.value;
+        solarsys.classList.value = ref;
+        e.parentElement.classList.toggle( 'active' );
+      } );
+    } )
+  }
 
 
-const planets = [
-  { Planet: "mercury" },
-  { Planet: "venus" },
-  {
-    Planet: "earth",
-    Moon: "moon"
-  },
-  { Planet: "mars" },
-  { Planet: "jupiter" },
-  { Planet: "saturn" },
-  { Planet: "uranus" },
-  { Planet: "neptune" } ];
+  const planets = [
+    { Planet: "mercury" },
+    { Planet: "venus" },
+    {
+      Planet: "earth",
+      Moon: "moon"
+    },
+    { Planet: "mars" },
+    { Planet: "jupiter" },
+    { Planet: "saturn" },
+    { Planet: "uranus" },
+    { Planet: "neptune" } ];
 
-const system = ( body ) => {
-  return `
+  const system = ( body ) => {
+    return `
       <div id="${ body.Planet }" class="orbit">
+        <div class="pos">
+          <div class="orbit">
             <div class="pos">
-              <div class="orbit">
-                <div class="pos">
-                  <div class="${ body.Moon }"></div>
-                </div>
-              </div>
-              <div class="planet">
-                <dl class="infos">
-                  <dt>${ body.Planet }</dt>
-                  <dd><span></span></dd>
-                </dl>
-              </div>
+              <div class="${ body.Moon }"></div>
             </div>
+          </div>
+          <div class="planet">
+            <dl class="infos">
+              <dt>${ body.Planet }</dt>
+              <dd><span></span></dd>
+            </dl>
+          </div>
+        </div>
       </div>`
-}
+  }
 
-document.getElementById( "insystem" ).innerHTML = `${ planets.map( system ).join( '' ) }`
+  document.getElementById( "insystem" ).innerHTML = `${ planets.map( system ).join( '' ) }`
 
-const dataz = ( body ) => {
-  return `<li><a id="button" class="${ body.Planet }" title="${ body.Planet }" href="#${ body.Planet }speed">${ body.Planet }</a></li>`
-}
-
-document.getElementById( "planetlist" ).innerHTML = `${ planets.map( dataz ).join( '' ) }`
+  document.getElementById( "planetlist" ).innerHTML += `${ planets.map( body => `<li><a id="button" class="${ body.Planet }" title="${ body.Planet }" href="#${ body.Planet }speed">${ body.Planet }</a></li>` ).join( '' ) }`
+} )();
